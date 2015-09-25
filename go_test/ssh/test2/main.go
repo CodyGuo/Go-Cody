903685636d582866c3c90dc83d5db224f3139f86
CodyGuo <guoxiao219@126.com>
2015-09-25 15:11:55 +0800

    增加http下载例子,有进度条

---
+++ go_test/ssh/test2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    // "fmt"
+    "golang.org/x/crypto/ssh"
+    // "io"
+    "log"
+    // "os"
+    "bytes"
+)
+
+func main() {
+    config := &ssh.ClientConfig{
+        User: "root",
+        Auth: []ssh.AuthMethod{
+            ssh.Password("123456"),
+        },
+    }
+    client, err := ssh.Dial("tcp", "10.10.4.100:22", config)
+    if err != nil {
+        panic(err)
+    }
+    // Each ClientConn can support multiple interactive sessions,
+    // represented by a Session.
+    defer client.Close()
+    // Create a session
+    session, err := client.NewSession()
+    if err != nil {
+        log.Fatalf("unable to create session: %s", err)
+    }
+    defer session.Close()
+
+    modes := ssh.TerminalModes{
+        ssh.ECHO:          0,     // disable echoing
+        ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+        ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+    }
+    if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+        log.Fatal(err)
+    }
+    w, err := session.StdinPipe()
+    if err != nil {
+        panic(err)
+    }
+
+    var b bytes.Buffer
+    session.Stdout = &b
+    err = session.Start(cmd)
+    if err != nil {
+        log.Println(err)
+    }
+
+    log.Println(b.String())
+
+    // w.Write([]byte("admin\n"))
+    // w.Write([]byte("admin2014\n"))
+    // w.Write([]byte("echo 2233 >>1.txt\n"))
+
+    // session.Wait()
+}
---
+++ go_test/test/down/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
+    "strings"
+    "time"
+
+    "github.com/cheggaaa/pb"
+)
+
+var (
+    url = flag.String("url", "", "The download file URL.")
+)
+
+func Usage() {
+    fmt.Printf("Usage of %s:\n  -url=\"http://www.xxx.com/file.exe\": The download file URL.\n", os.Args[0])
+    os.Exit(1)
+}
+
+func main() {
+    flag.Parse()
+
+    if os.Args == nil || *url == "" {
+        Usage()
+    }
+
+    urlMap := strings.Split(*url, "/")
+    fileName := urlMap[len(urlMap)-1]
+
+    resp, err := http.Get(*url)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    if resp.StatusCode == http.StatusOK {
+        log.Printf("[INFO] 正在下载: [%s]", fileName)
+        fmt.Print("\n")
+
+        downFile, err := os.Create(fileName)
+        if err != nil {
+            log.Fatal(err)
+        }
+        i, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
+        sourceSiz := int64(i)
+        source := resp.Body
+
+        bar := pb.New(int(sourceSiz)).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+        bar.Start()
+        bar.ShowFinalTime = true
+        bar.SetMaxWidth(80)
+
+        writer := io.MultiWriter(downFile, bar)
+
+        io.Copy(writer, source)
+        bar.Finish()
+
+        fmt.Print("\n")
+        log.Printf("[INFO] [%s]下载成功.", fileName)
+
+    } else {
+        fmt.Print("\n")
+        log.Printf("[ERROR] [%s]下载失败,%s.", fileName, resp.Status)
+    }
+
+}
