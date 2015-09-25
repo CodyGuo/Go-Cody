786ba8d9337b2173dd1a41731e7983dfbb7e5530
CodyGuo <guoxiao219@126.com>
2015-09-25 19:45:09 +0800

    优化http下载，忘记关闭os.Create

--- go_test/test/down/main.go
+++ go_test/test/down/main.go
@@ -30,9 +30,11 @@ func main() {
         Usage()
     }
 
+    // 解析/后的文件名字
     urlMap := strings.Split(*url, "/")
     fileName := urlMap[len(urlMap)-1]
 
+    // 解析带? = fileName 的文件名字
     if strings.Contains(fileName, "=") {
         splitName := strings.Split(fileName, "=")
         fileName = splitName[len(splitName)-1]
@@ -43,6 +45,7 @@ func main() {
         log.Fatal(err)
     }
 
+    // 判断get url的状态码, StatusOK = 200
     if resp.StatusCode == http.StatusOK {
         log.Printf("[INFO] 正在下载: [%s]", fileName)
         fmt.Print("\n")
@@ -51,25 +54,36 @@ func main() {
         if err != nil {
             log.Fatal(err)
         }
+        // 不要忘记关闭打开的文件.
+        defer downFile.Close()
+
+        // 获取下载文件的大小
         i, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
         sourceSiz := int64(i)
         source := resp.Body
 
+        // 创建一个进度条
         bar := pb.New(int(sourceSiz)).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
-        bar.Start()
 
+        // 显示下载速度
         bar.ShowSpeed = true
+
+        // 显示剩余时间
+        bar.ShowTimeLeft = true
+
+        // 显示完成时间
         bar.ShowFinalTime = true
+
         bar.SetMaxWidth(80)
 
-        writer := io.MultiWriter(downFile, bar)
+        bar.Start()
 
+        writer := io.MultiWriter(downFile, bar)
         io.Copy(writer, source)
         bar.Finish()
 
         fmt.Print("\n")
         log.Printf("[INFO] [%s]下载成功.", fileName)
-
     } else {
         fmt.Print("\n")
         log.Printf("[ERROR] [%s]下载失败,%s.", fileName, resp.Status)
