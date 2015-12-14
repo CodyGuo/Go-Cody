682de27116cd31b3d9623d3cb53612069880a834
CodyGuo <guoxiao219@126.com>
2015-12-14 19:29:02 +0800

    解决fmt.Scanf缓冲区问题

--- go_test/basic/fmt/f1/main.go
+++ go_test/basic/fmt/f1/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"os"
+	"syscall"
 )
 
 func main() {
-    var a int
-    var f float32
-    var str string
+	var a int
+	var f float32
+	var str string
 
-    fmt.Println("Please input: ")
-    fmt.Scanf("%d, %f, %s", &a, &f, &str)
+	fmt.Print("Please input : ")
+	n, _ := fmt.Scanf("%d %f %s", &a, &f, &str)
+	fmt.Println("Output: ", a, f, str, n)
 
-    fmt.Println("Output: ", a, f, str)
+	fmt.Println("----------------------------")
+
+	// 初始化stdin
+	os.Stdin = os.NewFile(uintptr(syscall.Stdin), "/dev/stdin")
+
+	var sIP, sMac string
+	fmt.Print("Please input: ")
+	fmt.Scanf("%s %s", &sIP, &sMac)
+
+	fmt.Println("Output: ", sIP, sMac)
 }
