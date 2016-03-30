d8547e0784c460743808507f484b963503c49c95
CodyGuo <guoxiao219@126.com>
2016-03-31 00:47:00 +0800

    并发获取代理地址

--- go_test/http/goquery/go2/main.go
+++ go_test/http/goquery/go2/main.go
@@ -4,71 +4,100 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
-	// "time"
+	"os"
+	"text/tabwriter"
 )
 
-type proxy struct {
-	country   string
-	ip        string
-	port      string
-	position  string
-	anonymous string
-	pTtype    string
-	time      string
+type Proxy struct {
+	ip         string
+	port       string
+	pTtype     string
+	speed      string
+	verifyTime string
 }
 
-func findProxy(url string) {
-	doc, err := goquery.NewDocument(url)
-	checkError(err)
+type ProxyModel struct {
+	proxy []Proxy
+	debug bool
+	*goquery.Document
+}
 
-	title := new(proxy)
-	var content = make([]proxy, 5, 10)
-
-	doc.Find("#ip_list").Each(func(i int, t *goquery.Selection) {
-		t.Find(".subtitle").Each(func(i int, s *goquery.Selection) {
-			if i == 0 {
-				title.country = s.Find("th").Eq(0).Text()
-				title.ip = s.Find("th").Eq(1).Text()
-				title.port = s.Find("th").Eq(2).Text()
-				title.position = s.Find("th").Eq(3).Text()
-				title.anonymous = s.Find("th").Eq(4).Text()
-				title.pTtype = s.Find("th").Eq(5).Text()
-				title.time = s.Find("th").Eq(6).Text()
-			}
-		})
-
-		t.Find("tbody").Each(func(i int, s *goquery.Selection) {
-			s.Find("tr").Each(func(n int, s *goquery.Selection) {
-				if !s.Find("tr").Is(".subtitle") || i != 0 {
-					coun, _ := s.Find("td > img").Attr("alt")
-					content = append(content, proxy{
-						coun,
-						s.Find("td").Eq(1).Text(),
-						s.Find("td").Eq(2).Text(),
-						s.Find("td").Eq(3).Text(),
-						s.Find("td").Eq(4).Text(),
-						s.Find("td").Eq(5).Text(),
-						s.Find("td").Eq(6).Text()})
-				}
-			})
-		})
-	})
-
-	fmt.Printf("%-4s %-16s %-6s %-12s %-6s %-10s \n", title.country, title.ip, title.port,
-		title.position, title.pTtype, title.time)
-
-	for _, value := range content {
-		if value.country == "Cn" {
-			fmt.Printf("%3s %16s %10s %10s %+10s %+10s \n", value.country, value.ip, value.port,
-				value.position, value.pTtype, value.time)
+func GetProxy(url string, page int) *ProxyModel {
+	urlList := []string{}
+	if page > 0 {
+		for i := 1; i <= page; i++ {
+			urlList = append(urlList, fmt.Sprintf("%s/%d", url, i))
 		}
 	}
+
+	var proxy *ProxyModel = &ProxyModel{}
+	fmt.Println("url:", urlList)
+	ok := make(chan bool)
+	for index, url := range urlList {
+		fmt.Printf("正在获取代理地址的URL [%d]: %s\n", index, url)
+		var err error
+		proxy.Document, err = goquery.NewDocument(url)
+		checkError(err)
+		go func() {
+			proxy.getProxy()
+			ok <- true
+			fmt.Printf("URL[%d] 已发送退出消息.\n", index)
+		}()
+	}
+
+	for index, _ := range urlList {
+		<-ok
+		fmt.Printf("URL[%d] 已收到退出消息.\n", index)
+	}
+
+	return proxy
+}
+
+func (p *ProxyModel) show() {
+	title := new(Proxy)
+	p.Find("#ip_list > tbody > tr").Each(func(i int, t *goquery.Selection) {
+		if i == 0 {
+			title.ip = t.Find("th").Eq(2).Text()
+			title.port = t.Find("th").Eq(3).Text()
+			title.pTtype = t.Find("th").Eq(6).Text()
+			title.speed = t.Find("th").Eq(7).Text()
+			title.verifyTime = t.Find("th").Eq(9).Text()
+		}
+	})
+
+	const format = "%v\t %v\t %v\t %v\t %v\t \n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 10, 3, ' ', 0)
+	fmt.Fprintf(tw, format, title.ip, title.port, title.pTtype, title.speed, title.verifyTime)
+	fmt.Fprintf(tw, format, "--------", "--------", "--------", "--------", "--------")
+
+	for _, v := range p.proxy {
+		fmt.Fprintf(tw, format, v.ip, v.port, v.pTtype, v.speed, fmt.Sprint(v.verifyTime))
+	}
+
+	fmt.Fprintf(tw, "已获取的代理地址数：%d\n", len(p.proxy))
+	tw.Flush()
+}
+
+func (p *ProxyModel) getProxy() {
+	p.Find("#ip_list > tbody > tr").Each(func(i int, t *goquery.Selection) {
+		if i > 0 {
+			speed, _ := t.Find("td > div").Attr("title")
+			p.proxy = append(p.proxy, Proxy{
+				t.Find("td").Eq(2).Text(),
+				t.Find("td").Eq(3).Text(),
+				t.Find("td").Eq(6).Text(),
+				speed,
+				t.Find("td").Eq(9).Text(),
+			})
+		}
+	})
 
 }
 
 func main() {
-	url := "http://www.xicidaili.com/"
-	findProxy(url)
+	url := "http://www.xicidaili.com/nn"
+	proxy := GetProxy(url, 3)
+	proxy.show()
 }
 
 func checkError(err error) {
