246ff1929c327662ff90df28845700ac40e18641
CodyGuo <guoxiao219@126.com>
2016-12-30 00:46:39 +0800

    exec

--- go_example/Test/2016/201612/29/exec/test1/main.go
+++ go_example/Test/2016/201612/29/exec/test1/main.go
@@ -2,36 +2,37 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
+	// "syscall"
 	"time"
+
+	_ "github.com/codyguo/godaemon"
+	"github.com/codyguo/win"
 )
 
 func main() {
-	cmd := exec.Command("ping", "-t", "10.10.2.1")
-	cmd.Stdout = os.Stdout
+	cmd := exec.Command("ping", "-t", "192.168.1.1")
+	// cmd.Stdout = os.Stdout
 	// inW, _ := cmd.StdinPipe()
 	cmd.Start()
 
 	go func() {
 		time.Sleep(3 * time.Second)
 		fmt.Println("退出程序中...", cmd.Process.Pid)
-		cmd.Process.Signal(syscall.SIGINT)
-		os.Exit(0)
-		cmd.Process.Signal(syscall.SIGQUIT)
-		cmd.Process.Signal(syscall.SIGTERM)
-		// cancel()
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
+		pid := cmd.Process.Pid
 
-		// Block until a signal is received.
-		s := <-c
-		fmt.Println("Got signal:", s)
-		time.Sleep(5 * time.Second)
-		cmd.Process.Signal(os.Interrupt)
+		handle := win.OpenProcess(uint32(win.PROCESS_ALL_ACCESS), true, uint32(pid))
+		hWnd := win.HWND(handle)
+		fmt.Println("进程pid：", pid, "handle: ", hWnd)
+
+		// win.SendMessage(hWnd, win.WM_CLOSE, 0, 0)
+
+		// inW.Write([]byte("{Ctrl}+{C}\r\n"))
+		// cmd.Process.Signal(syscall.SIGINT)
+		// cmd.Process.Signal(syscall.SIGQUIT)
+		// cmd.Process.Signal(syscall.SIGTERM)
+		// cmd.Process.Signal(os.Interrupt)
 	}()
 
-	// cmd.Wait()
+	cmd.Wait()
 }
